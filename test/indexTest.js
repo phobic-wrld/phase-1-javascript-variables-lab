4314f915b3492ad8ba7623d927129aaac6f32c69
joy <blackbearmode1@gmail.com>
2024-03-22 19:47:49 +0300

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,8 +6,10 @@ const path = require('path')
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
 describe('index.js', function () {
+ // there's stuff in here, too
   describe('companyName', function () {
     it('is set as Scuber', function () {
+      // tests are here
       expect(companyName).to.equal('Scuber');
     });
 
